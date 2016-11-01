404ab88463d87ca89e37b9592aee8f765c991a77
mTX <851528325@qq.com>
2016-11-01 21:36:36 +0800

    [Improvement]优化Na与H5交互，优化Toast工具类

--- app/src/main/java/com/mtx/androidcommonutil/ui/NaH5Activity.java
+++ app/src/main/java/com/mtx/androidcommonutil/ui/NaH5Activity.java
@@ -41,6 +41,8 @@ public class NaH5Activity extends AppCompatActivity implements View.OnClickListe
     private void updateUI() {
 //        -----打开本包内main/assets目录下的index.html文件
         mWebView.loadUrl("file:///android_asset/index.html"); //放在assets的html需加上  android_asset/
+//        mWebView.loadData(mUrlData, "text/html; charset=UTF-8", null);    //mUrlData字符串资源为html格式，且没有任何非法字符空格，回车，之类
+
 
 //        -----打开本地sd卡内的index.html文件
         //mWebView.loadUrl("content://com.android.htmlfileprovider/sdcard/index.html");
--- app/src/main/java/com/mtx/androidcommonutil/util/ToastUtil.java
+++ app/src/main/java/com/mtx/androidcommonutil/util/ToastUtil.java
@@ -5,20 +5,33 @@ import android.text.TextUtils;
 import android.widget.Toast;
 
 /**
+ * 优化后的Toast工具类，短时间内连续调用，Toast会把之前的先消失掉，立即展现
  * Created by lishaoming on 16/10/30.
  */
 public class ToastUtil {
+
+    private static Toast sToast;
+
     public static void show(Context context, String string) {
         if (context == null || TextUtils.isEmpty(string)) {
             return;
         }
-        Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+        if (sToast != null) {
+            sToast.cancel();
+        }
+        sToast = Toast.makeText(context, string, Toast.LENGTH_SHORT);
+        sToast.show();
     }
 
     public static void show(Context context, int stringId) {
         if (context == null) {
             return;
         }
-        Toast.makeText(context, stringId, Toast.LENGTH_SHORT).show();
+
+        if (sToast != null) {
+            sToast.cancel();
+        }
+        sToast = Toast.makeText(context, stringId, Toast.LENGTH_SHORT);
+        sToast.show();
     }
 }
